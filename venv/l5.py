0e3b8cffed15943b779b9e79dcaec114f85ab570
SERGIO CANALES MARTINEZ <s.canales@sis.virtual.uniandes.edu.co>
2020-03-12 13:49:32 -0500

    avance punto2

--- venv/l5.py
+++ venv/l5.py
@@ -23,7 +23,7 @@ ejemplos de entrada para punto 1:
 #es necesario correr esta función sin cellblock, para que ingresar la matriz sea más fácil
 import numpy as np
 def punto1():
-    print("ingrese en la primera línea el tamaño de la matriz A y en las siguientes las filas de la matriz con columnas separadas por espacios (referir a las ejemplols)")
+    print("ingrese en la primera línea el tamaño de la matriz A y en las siguientes las filas de la matriz con columnas separadas por espacios (referir a las ejemplos)")
     n=int(input())#numero de filas
     mat=[]
     A = []
@@ -51,7 +51,39 @@ def punto1():
             cIndex -= 1
         coefficient.append((mat[fIndex][-1]-sumaLocal)/mat[fIndex][cIndex])#hace la siguiente operación ax + suma(b,c,d...) = e -> x = e-suma(b,c,d...)/a
     coefficient.reverse()#como los encontró de abajo para arrba, toca voltearlo
-    print("los valores que yo calculé son: {} , los de numpy son {}".format(coefficient,np.linalg.solve(A,b)))
+    print("los valores que yo calculé son:\n {} \nlos de numpy son: \n {}".format(coefficient,np.linalg.solve(A,b)))
 
-punto1()
+
+
+def punto2():
+    print("ingrese en la primera línea el tamaño de la matriz A y en las siguientes las filas de la matriz con columnas separadas por espacios (referir a las ejemplos)")
+    n=int(input())#numero de filas
+    mat=[]
+    A = []
+    b = []
+    for _ in range(n):
+        mat.append(list(map(int, input().split())))#coge cada linea y la convierte a un arreglo de enteros, luego las agrega a la matriz mat
+        A.append(mat[_][:-1])#meterle todos los datos menos el último
+        b.append([mat[_][-1]])#meterle el último dato
+    #algoritmo
+    for fIndex in range(n):
+        for cIndex in range(len(mat[0])):
+            if cIndex ==fIndex:
+                break
+            coefficient = (mat[fIndex][cIndex]/mat[cIndex][cIndex])#coeficiente para que se ponga cero en el CIndex y convierta el resto de la fila
+            mat[fIndex] = [mat[fIndex][currentC]-coefficient*mat[cIndex][currentC] for currentC in range(len(mat[fIndex]))]#crea una nueva lista(fila) restando cada data (pareja fila-columna) por el coeficiente multiplicado con la fila y columna requerida
+    #hasta acá es igual al punto 1
+
+
+    for fIndex in range(len(mat)-1,-1,-1):
+        cIndex = len(mat[0])-2#para no contar el del vector b
+        while cIndex > fIndex:
+           # mat[fIndex][cIndex] = mat[fIndex][cIndex] - (mat[cIndex][cIndex]/mat[fIndex][cIndex])*mat[currentC][cIndex]
+            cIndex -= 1
+    print(mat)
+    coefficients = [mat[i][-1]/mat[i][i] for i in range(len(mat))]#recorre la diagonal principal y calcula el valor del vector b tal que la identidad de A sea 1
+
+    print("los valores que yo calculé son:\n {} \nlos de numpy son: \n {}".format(coefficients,np.linalg.solve(A,b)))
+
+punto2()
 #para el punto 4 y 5 toca usar polares. rcos y rsen
