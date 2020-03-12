37a3f1e63723ac00f029f69eb4bc7198fe68ac33
sergio <s.canales@uniandes.edu.co>
2020-03-11 22:32:28 -0500

    punto 1 completo

--- venv/l5.py
+++ venv/l5.py
@@ -8,24 +8,30 @@
 '''
 ejemplos de entrada para punto 1:
 
-4
+3
 1 2 3 3
 2 6 9 2
 14 9 5 3
-1 2 3 4
+
 
 3
-3 2 1 
-2 6 9 
-14 9 5 
+3 2 1 1
+2 6 9 1
+1 9 5 1
 
 '''
+#es necesario correr esta función sin cellblock, para que ingresar la matriz sea más fácil
+import numpy as np
 def punto1():
     print("ingrese en la primera línea el tamaño de la matriz A y en las siguientes las filas de la matriz con columnas separadas por espacios (referir a las ejemplols)")
     n=int(input())#numero de filas
     mat=[]
+    A = []
+    b = []
     for _ in range(n):
         mat.append(list(map(int, input().split())))#coge cada linea y la convierte a un arreglo de enteros, luego las agrega a la matriz mat
+        A.append(mat[_][:-1])#meterle todos los datos menos el último
+        b.append([mat[_][-1]])#meterle el último dato
     #algoritmo
     for fIndex in range(n):
         for cIndex in range(len(mat[0])):
@@ -33,8 +39,19 @@ def punto1():
                 break
             coefficient = (mat[fIndex][cIndex]/mat[cIndex][cIndex])#coeficiente para que se ponga cero en el CIndex y convierta el resto de la fila
             mat[fIndex] = [mat[fIndex][currentC]-coefficient*mat[cIndex][currentC] for currentC in range(len(mat[fIndex]))]#crea una nueva lista(fila) restando cada data (pareja fila-columna) por el coeficiente multiplicado con la fila y columna requerida
-    print(mat)
 
+    #encontrar coeficientes
+    coefficient = []
+    for fIndex in range(len(mat)-1,-1,-1):
+        cIndex = len(mat[0])-2#para no contar el del vector b
+        sumaLocal = 0 # aquí se guarda el valor que se va calculado de cada fila para poder calcular el coeficiente correspondiente
+        #el algoritmo recorre el triangulo superior, desde la esquina de abajo, hasta la esquina de arriba a la izquierda. se calcula el de más abajo, luego ese se usa para calcular de de arriba, luego esos dos para calcular el de más arriba y así sucesivamente
+        while cIndex > fIndex:
+            sumaLocal += coefficient[len(mat[0])-2-cIndex]*mat[fIndex][cIndex]#usa los coeficientes ya encontrados para sumar todos los escalares hasta el "pivote"
+            cIndex -= 1
+        coefficient.append((mat[fIndex][-1]-sumaLocal)/mat[fIndex][cIndex])#hace la siguiente operación ax + suma(b,c,d...) = e -> x = e-suma(b,c,d...)/a
+    coefficient.reverse()#como los encontró de abajo para arrba, toca voltearlo
+    print("los valores que yo calculé son: {} , los de numpy son {}".format(coefficient,np.linalg.solve(A,b)))
 
 punto1()
 #para el punto 4 y 5 toca usar polares. rcos y rsen
