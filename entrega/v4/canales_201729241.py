b187165f253d4d86d04b203a802bf80952b4e24e
Sergio <sergiocanalesm1@gmail.com>
2020-04-15 12:09:35 -0500

    soluciones multivariables

--- entrega/v4/canales_201729241.py
+++ entrega/v4/canales_201729241.py
@@ -1,32 +1,4 @@
-## secante
-def funcion1(x):
-    return - (np.sqrt(3.0*x)**(2.0/5.0))+(x**3.0)*np.cos(3.0*x)+4.0*(x**2.0) - 7.0
-def funcion2(x):
-    return -x**(0.25)+np.sin(3.5*x)+4*x**(0.5)+2*x - 5
-def secante(f,tol):
-    x0 = 2.0
-    x1 = 1.9
-    tolf = tol
-    tolx = tolf
-    x2prev = x1
-    iteraciones = 0
-    while True:
-        iteraciones +=1
-        x2 = x1 - (f(x1) * (x1 - x0) / (f(x1) - f(x0)))
-        if np.abs(x2 - x2prev) <= tolx:
-            break
-        if np.abs(f(x2)) <= tolf:
-            break
-        x0 = x1
-        x1 = x2
-        x2prev = x2
-    print("Secante: con {} iteraciones, {} de tolerancia, la raiz es: {}  ".format(iteraciones, tolx, x2))
 
-print("f1")
-secante(funcion1,10**-15)
-print("f2")
-secante(funcion2,10**-5)
-secante(funcion2,10**-15)
 
 ## secante tasa de convergencia
 import numpy as np
@@ -66,5 +38,10 @@ def secante(f,tol):
     plt.plot(iter_array,tasas_convergencias)
     plt.plot(iter_array,tasas_convergencias,"or")
     plt.show()
+
 print("f1")
 secante(funcion1,10**-10)
+secante(funcion1,10**-15)
+print("f2")
+secante(funcion2,10**-5)
+secante(funcion2,10**-15)
